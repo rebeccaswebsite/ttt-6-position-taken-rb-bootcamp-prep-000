2bf6ea8857625d34139cee3728653bcb23196074
Rebecca Huseyin <rebecca_huseyin@outlook.com>
2019-03-10 22:41:50 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,4 @@
 def position_taken?(board,index)
-  #if board[index] == " " || board[index] == "" || board[index] = "nil"
-    #return false
- # elsif board[index] == "x" || board[index] == "o"
-   # return true
- # end
   if board[index] == "X" || board[index] == "O"
     return true
   elsif board[index] == " " || board[index] == "" || board[index] = "nil"
